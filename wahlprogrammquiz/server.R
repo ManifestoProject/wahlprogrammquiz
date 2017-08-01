41720dda41954c91fd48e64a4c27222beefc794d
Jirka Lewandowski <jirka.lewandowski@wzb.eu>
2017-08-01 16:32:31 +0200

    Connect to database and compute answer distribution

--- wahlprogrammquiz/server.R
+++ wahlprogrammquiz/server.R
@@ -1,9 +1,23 @@
 library(shiny)
 library(readr)
-library(dplyr)
+library(dplyr) ## developed with package version 0.7
 library(magrittr)
 
+library(DBI) ## developed with package version 0.7
+library(RSQLite) ## developed with package version 2.0
+
 ROOT_URL = "localhost:8020" ## TODO replace with final URL
+RESPONSES = "responses"
+db_connection <- dbConnect(RSQLite::SQLite(), "wahlprogrammquiz.sqlite")
+if (!db_has_table(db_connection, RESPONSES)) {
+  dbWriteTable(db_connection,
+               RESPONSES,
+               data_frame(session_id = character(),  ## This is a schema definition
+                          sentence_id = integer(),   ## for the database table
+                          time_stamp = character(),
+                          answer = character()
+                          ))
+} 
 
 ### iff function from gitlabr and manifestoR; this is copied in order to not import these packages
 iff <- function (obj, test, fun, ...) {
@@ -46,6 +60,13 @@ shinyServer(function(input, output) {
   
   selected_answer <- reactive(input$answer_select) ## TODO this is likely replaced by more complex UI input processing?
   event_next <- reactive(input$button_next)
+  
+  answer_distribution <- reactive( ## This reacts on sentence_id, but should only be visible after selecting an answer
+    db_connection %>%
+      tbl(RESPONSES) %>%
+      dplyr::filter(sentence_id == !!as.numeric(state$sentence_id)) %>%
+      count(answer)
+  )
   
   ## event observers
   observeEvent(event_next(), {
