e6eed4cedd7871a5758f1bdf7b3040e01a1c54b7
Jirka Lewandowski <jirka.lewandowski@wzb.eu>
2017-08-01 16:54:23 +0200

    Buttons modify the answer_select shiny input value

--- wahlprogrammquiz/www/wahlprogrammquiz.js
+++ wahlprogrammquiz/www/wahlprogrammquiz.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
  $( "#linkeButton" ).click(function() {
-   Shiny.onInputChange("linkeButton", "");
+   Shiny.onInputChange("answer_select", "41223");
  });
   $( "#grueneButton" ).click(function() {
-   Shiny.onInputChange("linkeButton", "");
+   Shiny.onInputChange("answer_select", "41113");
  });
   $( "#spdButton" ).click(function() {
-   Shiny.onInputChange("linkeButton", "");
+   Shiny.onInputChange("answer_select", "41320");
  });
   $( "#cduButton" ).click(function() {
-   Shiny.onInputChange("linkeButton", "");
+   Shiny.onInputChange("answer_select", "41521");
  });
    $( "#fdpButton" ).click(function() {
-   Shiny.onInputChange("linkeButton", "");
+   Shiny.onInputChange("answer_select", "42420");
  });
   $( "#afdButton" ).click(function() {
-   Shiny.onInputChange("linkeButton", "");
+   Shiny.onInputChange("answer_select", "41953");
  });
 });
