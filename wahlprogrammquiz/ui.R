3679aa2dbe7e632ba40604a3d6e5ad777e96b77b
Paul Muscat <paul.muscat@wzb.eu>
2017-08-01 16:18:44 +0200

    added buttons (clickable images) and corresponding js, and a stylesheet

--- wahlprogrammquiz/ui.R
+++ wahlprogrammquiz/ui.R
@@ -1,9 +1,36 @@
 library(shiny)
+library(shinyBS)
 
 shinyUI(fluidPage(
+  includeCSS("www/wahlprogrammquiz.css"),
   
   # Application title
   titlePanel("Wahlprogrammquiz"),
   
-  p("Hello manifesto!")
+  p("Hello manifesto!"),
+  
+  span(id = "buttonsSpan",
+       div(id="linkeSpan", class = "buttonDiv",
+            img(id = "linkeButton", src = "images/linkeL.png")
+            ),
+       div(id="grueneSpan", class = "buttonDiv",
+            img(id = "grueneButton", src = "images/grueneL.png")
+            ),
+       div(id="spdSpan", class = "buttonDiv",
+            img(id = "spdeButton", src = "images/spdL.png")
+            ),
+       div(id="cduSpan", class = "buttonDiv",
+            img(id = "cdueButton", src = "images/cduL.png")
+            ),
+       div(id="fdpSpan", class = "buttonDiv",
+            img(id = "fdpButton", src = "images/fdpL.png")
+            ),
+       div(id="afdSpan", class = "buttonDiv",
+            img(id = "afdButton", src = "images/afdL.png")
+            )
+       ),
+
+  textOutput("statement")
+  
+  
 ))
---
+++ wahlprogrammquiz/www/wahlprogrammquiz.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+ $( "#linkeButton" ).click(function() {
+   Shiny.onInputChange("linkeButton", "");
+ });
+  $( "#grueneButton" ).click(function() {
+   Shiny.onInputChange("linkeButton", "");
+ });
+  $( "#spdButton" ).click(function() {
+   Shiny.onInputChange("linkeButton", "");
+ });
+  $( "#cduButton" ).click(function() {
+   Shiny.onInputChange("linkeButton", "");
+ });
+   $( "#fdpButton" ).click(function() {
+   Shiny.onInputChange("linkeButton", "");
+ });
+  $( "#afdButton" ).click(function() {
+   Shiny.onInputChange("linkeButton", "");
+ });
+});
