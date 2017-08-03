66adda6280c8d91b526a323146fd3fe4bb76f125
merz <nicolas.merz@wzb.eu>
2017-08-03 11:03:48 +0200

    random sampling done (not yet checked sentences manually)

--- R/sample_sentences.R
+++ R/sample_sentences.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(tidytext)
 library(stringr)
 library(zoo)
+library(xlsx)
 
 #"sentence_id","text","context_before","context_after","party"
 
@@ -12,11 +13,11 @@ library(zoo)
 partynames <- tribble(
   ~doc_id, ~partyabbrev,~party,~partycolor,~partyorder,
   "union.txt","CDU/CSU","41521","black",1,
-  "spd.txt","SPD","red","41320",2,
-  "linke.txt","LINKE","purple","41223",3,
-  "gruene.txt","GRÜNE","darkgreen","41113",4,
-  "fdp.txt", "FDP","gold","41420",5,
-  "afd.txt", "AfD","blue","41953",6
+  "spd.txt","SPD","41320","red",2,
+  "linke.txt","LINKE","41223","purple",3,
+  "gruene.txt","GRÜNE","41113","darkgreen",4,
+  "fdp.txt", "FDP","41420","gold",5,
+  "afd.txt", "AfD","41953","blue",6
 )
 
 replacement_char <- "XXXXX"
@@ -24,6 +25,8 @@ no_sent_endings <- c("Vgl\\.","z\\.B\\.","Abs\\.","Art\\.","u\\.a\\.","z\\.b\\."
 
 no_sent_ending_xxx <- str_replace_all(no_sent_endings,"(\\.)",replacement_char)                   
 replacement_list <- setNames(no_sent_ending_xxx,no_sent_endings)
+
+## paragraphs
 
 program_lines <- readtext("programs/*.txt", ignore_missing_files = FALSE, encoding="UTF-8") %>%
   left_join(partynames) %>%
@@ -38,7 +41,9 @@ program_lines <- readtext("programs/*.txt", ignore_missing_files = FALSE, encodi
   mutate(doc_line_id = row_number()) %>%
   ungroup() 
 
-program_sentences <- program_lines %>%
+## sentences
+
+sentences_lines <- program_lines %>%
   mutate(
     lines = str_replace_all(lines,replacement_list),
     lines = str_replace_all(lines,"(?<=[A-Z])\\.",replacement_char),
@@ -59,11 +64,26 @@ program_sentences <- program_lines %>%
   ) %>%
   left_join(program_lines %>% select(lines,corpus_line_id),by=c("corpus_line_id"="corpus_line_id")) %>%
   rename(paragraph=lines)
+
+## context
+
+program_sentences <- map2_chr(sentences_lines$paragraph,
+                              sentences_lines$sentence, 
+                    ~ str_replace_all(.x,fixed(.y),"SENTENCEHERE")) %>%
+  tibble(context=.) %>% 
+  bind_cols(sentences_lines) %>%
+  mutate(
+    context_after = str_replace_all(context,".*SENTENCEHERE",""),
+    context_before = str_replace_all(context,"SENTENCEHERE.*","")
+  ) %>% 
+  select(-context) %>%
+  rename(text=sentence)
   
-## context after, context before
-## seed setzen
-## not yet ready
+
+set.seed(42)
 sample_sentences <- program_sentences %>% filter(heading_order < 1) %>%
-  sample_n(20) %>% 
-  write_csv("sample.csv")
+  group_by(partyabbrev) %>%
+  sample_n(20) %T% 
+  write_csv("wahlprogrammquiz/sample.csv") %>%
+  write_xlsx("wahlprogrammquiz/sample.xlsx")
 
